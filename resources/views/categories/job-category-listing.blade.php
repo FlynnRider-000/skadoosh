bff305f7c686052f3543ecb19e6c90f3e23c012e
Devin <89885391+Superdev0624@users.noreply.github.com>
2022-02-25 15:06:15 +0200

    carousel issue

--- resources/views/categories/job-category-listing.blade.php
+++ resources/views/categories/job-category-listing.blade.php
@@ -122,15 +122,18 @@
 
         loop:false,
         margin:10,
-        nav:true,
+        nav:false,
         navText: ['<i class="fa fa-angle-left" aria-hidden="true"></i>', '<i class="fa fa-angle-right" aria-hidden="true"></i>'],
         responsiveClass:true,
         responsive:{
             0:{
-                items:1,
-            },
+                items:1.2,
+            },            
             600:{
-                items:2,
+                items:2.2,
+            },
+            800:{
+                items:3,
             },
             1000:{
                 items:4,
